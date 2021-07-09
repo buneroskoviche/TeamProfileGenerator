97596ac3dbaf563301fe1fdf64921a60b8c2b7f7
Buney <dpkinser93@gmail.com>
2021-07-08 18:49:47 -0600

    subclasses defined, and init func creates manager position

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // Require dependencies
 const inquirer = require('inquirer');
 const fileSystem = require('fs');
-const Manager = require('./lib/manager');
+const Manager = require('./lib/Manager');
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/intern');
+const Engineer = require('./lib/engineer');
 
 // Define a funciton that starts the app
-const init = () => {
-    console.log('getting started');
-    const manager = new Manager;
+const  init = async () => {
+    const employeesArr = [];
+    console.log('Welcome to the Team Profile Generator!\nLet\'s get started by creating the manager position.')
+    const manager = new Manager();
+    await manager.build();
+    employeesArr.push(manager);
 }
+
+// Define a function that starts the menu
+// const menuStart = async () => {
+//     await inquirer.prompt({
+
+//     });
+// }
 
 // Start the app
 init();
